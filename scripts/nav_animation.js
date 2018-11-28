477e515296067bf158a454d40f825c8aebd9348d
b <final_project_1950@protonmail.com>
2018-11-27 22:40:32 -0800

    added index.html and mobile nav animations (also created the layout for the header)

---
+++ scripts/nav_animation.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+	var menuButton = document.getElementsByClassName('btn-menu')[0];
+
+	menuButton.onclick = () => {
+		var navButtons = document.getElementsByClassName('nav_buttons'),
+			c = 0,
+			interval;
+
+		if ( menuButton.className.indexOf('menu_clicked') < 0 ) {
+			menuButton.className += "menu_clicked";
+			interval = setInterval(() => {
+				console.log("opening");
+				navButtons[c].style.right = "0px";
+				c++;
+				if (c >= 5 ) {
+					clearInterval(interval);
+				}
+			}, 50);
+		}else {
+			menuButton.className = "btn-menu";
+			interval = setInterval(() => {
+				console.log("closing");
+				navButtons[4-c].style.right = "-300px";
+				c++;
+				if (c >= 5 ) {
+					clearInterval(interval);
+				}
+			}, 50);
+		}
+	}
+}
